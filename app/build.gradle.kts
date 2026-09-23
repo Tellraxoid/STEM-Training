24dd19f2b23d9a6d43473829a72912449a6840c7
Tellraxoid <tellrax1990@gmail.com>
2026-09-23 19:51:43 +0700

    Bump app version to 1.10.2 beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.stem.stemtraining"
         minSdk = 26
         targetSdk = 37
-        versionCode = 30
-        versionName = "1.10.1-beta"
+        versionCode = 31
+        versionName = "1.10.2-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
